34cff99295ae47c020cc416995c0db3e55409f47
lszlo <jesusrodriguezgarcia97@gmail.com>
2019-02-07 17:31:02 +0100

    #21 luces

---
+++ Assets/Scripts/AudioSyncLuz.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSyncLuz : AudioSyncer
+{
+    public Color[] colores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void OnBeat()
+    {
+        base.OnBeat();
+        cambiarLuz();
+
+
+    }
+    void cambiarLuz()
+    {
+        GameObject[] luces = GameObject.FindGameObjectsWithTag( "Luz");
+       
+        for (int i = 0; i < luces.Length; i++)
+        {
+            int indiceColor = Random.Range(0, colores.Length - 1);
+            luces[i].GetComponent<SpriteRenderer>().color = colores[indiceColor];
+        }
+    }
+
+}
