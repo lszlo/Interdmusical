23276b3d6652cd7c4f6a833e23f1efa77df4e3e3
lszlo <33127492+lszlo@users.noreply.github.com>
2019-02-13 16:21:39 +0100

    Meter escenas en el build setting

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public Animator animacionInterface;
+    public bool pausa = false;
+    public int numeroEscena;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausa = !pausa;
+            animacionInterface.SetBool("visible", pausa);
+        }
+        if (pausa)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+	}
+    //poner el scene manager ese arriba UwU 
+    public void RecargaNivel()
+    {
+        SceneManager.LoadScene(numeroEscena);
+    }
+}
