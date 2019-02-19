62a61cbb7df8df848dc6be90806a8caa6a1f52a9
lszlo <33127492+lszlo@users.noreply.github.com>
2019-02-19 18:21:04 +0100

    Menu

---
+++ Assets/LuzCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LuzCanvasController : AudioSyncer
+{
+    public Color[] colores;
+
+    public override void OnBeat()
+    {
+        base.OnBeat();
+        cambiarLuz();
+
+
+    }
+    //hola
+    void cambiarLuz()
+    {
+        GameObject[] luces = GameObject.FindGameObjectsWithTag("Luz");
+        // Debug.Log("cambiandoluces");
+
+        for (int i = 0; i < luces.Length; i++)
+        {
+            int indiceColor = Random.Range(0, colores.Length - 1);
+            luces[i].GetComponent<Image>().color = colores[indiceColor];
+        }
+    }
+
+}
