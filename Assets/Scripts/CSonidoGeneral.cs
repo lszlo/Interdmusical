8800a54741c0f80b4c306b0670e12a602470ab63
juantrono <juantrono@gmail.com>
2019-02-19 17:43:00 +0100

    Slider volumen

---
+++ Assets/Scripts/CSonidoGeneral.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSonidoGeneral : MonoBehaviour
+{
+    AudioSource sonidoGeneral;
+
+    float volumen = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sonidoGeneral = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       sonidoGeneral.volume = volumen; 
+    }
+
+    public void SetVolume (float vol){
+        volumen = vol;
+    }
+}
