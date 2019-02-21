4d56f6e5f2450e0085bd74afda8318d644b9a4b6
QuinnFortesque <joaquinrodriguezmartos7@gmail.com>
2019-02-21 16:11:09 +0100

    #115BotonStart
    Actualmente, está creado el botón de start. al que si pulsas empieza una cuenta atrás. Cuando llega a 0, hace falta poner que empieza el nivel.

--- Assets/Scripts/CountDownStart.cs
+++ Assets/Scripts/CountDownStart.cs
@@ -1,23 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class CountDownStart : MonoBehaviour
 {
-    public Text contador;
+    private Text contador;
     private float tiempo = 3f;
+    private bool start;
+    private bool eventoLanzado = false;
+
+    public UnityEvent OnCountdownEnd;
 
     // Start is called before the first frame update
     void Start()
     {
+        contador = GetComponent<Text>();
         contador.text = " " + tiempo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        tiempo -= Time.deltaTime;
-        contador.text = " " + tiempo.ToString("f0");
+        if (start)
+        {
+
+            tiempo -= Time.deltaTime;
+            if (tiempo > 0)
+                contador.text = " " + tiempo.ToString("f0");
+            else{ 
+                if (!eventoLanzado)
+                    OnCountdownEnd.Invoke();
+            }
+
+        }
+
+    }
+
+   public void activador()
+    {
+        start = true;
     }
 }
